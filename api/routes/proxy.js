e044ec62de2c5ad575a68a5a24cd84fdece8809f
Devonte <devo.tox.89@gmail.com>
2016-10-20 16:59:56 +0100

    update proxy

--- api/routes/proxy.js
+++ api/routes/proxy.js
@@ -6,28 +6,26 @@ const router = express.Router();
 
 const _ = require('lodash');
 
-router.get('/', (req, res) => {
-	res.send('Proxy Needs POST');
-});
+router.all('/', (req, res) => {
+	let data = Object.assign({}, req.body, req.query);
 
-router.post('/', (req, res) => {
-	if(!req.body.url) {
+	if(!data.url) {
 		return res.status(500).send('Need to send a url in the request body');
 	}
 
 	let config = Object.assign({
-		url: req.body.url,
-		aws: req.body.aws,
-		auth: req.body.auth,
-		form: req.body.form,
-		oauth: req.body.oauth,
-		method: req.body.method,
-		formData: req.body.formData,
-		body: req.body.data || null,
-		qs: req.body.params || null,
-		headers: req.body.headers || null,
-		json: _.isObject(req.body.data) ? true : false
-	}, req.body.config);
+		url: data.url,
+		aws: data.aws,
+		auth: data.auth,
+		form: data.form,
+		oauth: data.oauth,
+		method: data.method,
+		formData: data.formData,
+		body: data.data || null,
+		qs: data.params || null,
+		headers: data.headers || null,
+		json: _.isObject(data.data) ? true : false
+	}, data.config);
 
 	request(config).pipe(res);
 });
